c4cdf37b6bfa93dd7970dca42353809b3ac1b76b
Matt Mondok <matt.mondok@gmail.com>
2024-07-17 12:56:22 -0400

    Moved to HTML

--- app/controllers/text_renders_controller.rb
+++ app/controllers/text_renders_controller.rb
@@ -4,9 +4,16 @@ class TextRendersController < ApplicationController
 
   # GET /text_renders or /text_renders.json
   def index
-    url = Rails.public_path + 'war-and-peace.pdf'
-    io     = open(url)
-    @reader = PDF::Reader.new(io)
+    # url = Rails.public_path + 'war-and-peace.pdf'
+    # url = Rails.public_path + 'war-and-peace.pdf'
+    # io     = open(url)
+    # @reader = PDF::Reader.new(io)
+    copies = params[:copies] || 1
+    content = File.read(Rails.public_path + "pg2600-images.html")
+    @content = ''
+    0.upto(copies.to_i).each do |p|
+      @content += content
+    end
   end
 
 end
