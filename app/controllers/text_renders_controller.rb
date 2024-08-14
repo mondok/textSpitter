ad9bf6aed42ba422ece823e469d8379dcf09cd80
Matt Mondok <matt.mondok@gmail.com>
2024-08-14 07:49:53 -0400

    Quick cleanup

--- app/controllers/text_renders_controller.rb
+++ app/controllers/text_renders_controller.rb
@@ -2,12 +2,7 @@ require 'open-uri'
 
 class TextRendersController < ApplicationController
 
-  # GET /text_renders or /text_renders.json
   def index
-    # url = Rails.public_path + 'war-and-peace.pdf'
-    # url = Rails.public_path + 'war-and-peace.pdf'
-    # io     = open(url)
-    # @reader = PDF::Reader.new(io)
     copies = params[:copies] || 1
     content = File.read(Rails.public_path + "pg2600-images.html")
     @content = ''
